c630bf102112459367aa9b02bb344fc14e6ea0e6
09dian <09dianm@gmail.com>
2024-10-20 09:52:18 +0700

    update_calculator

--- calculator.py
+++ calculator.py
@@ -12,34 +12,30 @@ input_pilihan =int(pilihan)
 def input_nilai():
    angka1= int(input("Masukan Angka Ke 1 : "))
    angka2= int(input("Masukan Angka Ke 2 : ")) 
+   return angka1,angka2
 
 if input_pilihan == 1:
-   print("=====Perkalian=====")
-   input_nilai()
-   hasil=angka1*angka2
-   print('Hasil Perkalian dari',angka1,'dibagi',angka2,'adalah = ',hasil)
+  print("=====Perkalian=====")
+  angka1, angka2= input_nilai()
+  hasil=angka1*angka2
+  print('Hasil Perkalian dari',angka1,'dibagi',angka2,'adalah = ',hasil)
 elif input_pilihan == 2:
        
    print("=====Pembagian=====")
-   angka1= int(input("Masukan Angka Ke 1 : "))
-   angka2= int(input("Masukan Angka Ke 2 : "))
+   angka1, angka2= input_nilai()
    hasil= angka1/angka2
    print('Hasil Pembagian dari',angka1,'dibagi',angka2,'adalah = ',hasil)
   
 elif input_pilihan == 3:
     print("=====Pertambahan=====")
-    angka1= int(input("Masukan Angka Ke 1 : "))
-    angka2= int(input("Masukan Angka Ke 2 : "))
+    angka1, angka2= input_nilai()
     hasil= angka1+angka2
     print('Hasil Penjumlahan dari',angka1,'ditambah',angka2,'adalah = ',hasil)
   
 elif input_pilihan == 4:
     print("=====Pengurangan=====")
-    angka1= int(input("Masukan Angka Ke 1 : "))
-    angka2= int(input("Masukan Angka Ke 2 : "))
+    angka1, angka2= input_nilai()
     hasil= angka1-angka2
-    print('Hasil Pengurangan dari',angka1,'dikurang',angka2,'adalah = ',hasil)
-
-      
+    print('Hasil Pengurangan dari',angka1,'dikurang',angka2,'adalah = ',hasil)  
 else:
     print("Pilihan tidak ada")
--- scraping.py
+++ scraping.py
@@ -4,8 +4,11 @@ from bs4 import BeautifulSoup
 # URL halaman web yang akan di-scrape
 url = 'https://www.smkn7garut.sch.id'
 
+# Menambahkan header User-Agent agar permintaan tampak berasal dari browser
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+
 # Mengirim permintaan GET ke URL
-response = requests.get(url)
+response = requests.get(url, headers=headers)
 
 # Memastikan permintaan berhasil (status code 200)
 if response.status_code == 200:
@@ -15,8 +18,11 @@ if response.status_code == 200:
     # Mem-parsing konten HTML dengan BeautifulSoup
     soup = BeautifulSoup(page_content, 'html.parser')
     
+    # Cek struktur halaman yang diambil
+    print(soup.prettify())  # untuk debugging, bisa dilihat struktur HTML
+    
     # Menemukan semua elemen yang berisi judul artikel
-    articles = soup.find_all('a', class_='post-title')
+    articles = soup.find_all('a', class_='post-title')  # pastikan class sesuai
     
     # Menampilkan judul artikel
     for article in articles:
